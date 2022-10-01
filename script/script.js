efd9e94c58f4287d8f23e98737d0adef67a237ab
AnastasiaBar <90132016+AnastasiaBar@users.noreply.github.com>
2022-10-01 12:33:01 +0300

    tests

---
+++ script/script.js
@@ -0,0 +1,7 @@
+//burger menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
